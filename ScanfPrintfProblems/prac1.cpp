08d2fcf6aee44ea86cedf70f6b03448e462fb35f
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-11-03 21:08:57 +0600

    Create prac1.cpp

---
+++ ScanfPrintfProblems/prac1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // 1. Sum of two numbers
+    
+    int number1, number2;
+    printf("Enter two number: ");
+    scanf("%d %d", &number1, &number2);
+    printf("The sum is: %d", number1 + number2);
+
+    return 0;
+}
