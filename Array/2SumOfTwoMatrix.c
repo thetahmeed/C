7b3efcc89a7baf8f6e2171a14fe2bd2de2c6bc0a
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-05-03 12:22:18 +0600

    Create 2SumOfTwoMatrix.c

---
+++ Array/2SumOfTwoMatrix.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(){
+    int array1[2][2], array2[2][2], resultArray[2][2], i, j, k;
+    // Int. the value of the first array
+    printf("Enter value of Matrix 1 (2*2):\n");
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d", &array1[i][j]);
+        }
+    }
+    // Int. the value of the second array
+    printf("\nEnter value of Matrix 2 (2*2):\n");
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d", &array2[i][j]);
+        }
+    }
+    // Calculating the result
+    printf("\nThe result is:\n");
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 2; j++)
+        {
+            resultArray[i][j] = resultArray[i][j] + (array1[i][k] * array2[k][j]);
+        }
+    }
+    // Printing the result
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d \t", resultArray[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
