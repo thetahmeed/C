26748d2942c7b2c93f180b4db5348279099bd2b3
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-05-26 12:26:46 +0600

    Create 5Pointer.c

---
+++ Pointer/5Pointer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void swap(int *a, int *b);
+void main(void){
+    int x=2, y=3;
+    swap(&x, &y);
+}
+void swap(int *a, int *b){
+    int temp=*a;
+    *a = *b;
+    *b = temp;
+    printf("%d %d", *a, *b);
+}
